96d6e63057b9e9ec5d2c247fa8a1d2cb5ceeb54e
AdnanJoraid <Adnanjoraidca@gmail.com>
2022-06-19 19:27:17 -0400

    Added network request and dispatch queue to update UI on main thread

--- Todo/Controller/ViewController.m
+++ Todo/Controller/ViewController.m
@@ -21,6 +21,7 @@ NSString *cellId = @"cellId";
     [super viewDidLoad];
     
     [self setupQuotes];
+    [self fetchQuotes];
     
     self.navigationItem.title = @"Quotes";
     self.navigationController.navigationBar.prefersLargeTitles = YES;
@@ -35,6 +36,40 @@ NSString *cellId = @"cellId";
     quote.author = @"Thomas Edison";
     quote.quote = @"Genius is one percent inspiration and ninety-nine percent perspiration.";
     [self.quotes addObject:quote];
+}
+
+- (void)fetchQuotes {
+    //URL: https://type.fit/api/quotes
+    NSURL *url = [NSURL URLWithString:@"https://type.fit/api/quotes"];
+    [[NSURLSession.sharedSession dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        NSError *err;
+        
+        NSArray *quotesJson = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&err];
+        
+        if (err) {
+            NSLog(@"Error: %@", err);
+            return;
+        }
+        NSMutableArray<Quote *> *quotesList = NSMutableArray.new;
+        for (NSDictionary *quoteDict in quotesJson) {
+            Quote *quoteItem = Quote.new;
+            NSString *author = quoteDict[@"author"];
+            NSString *quote = quoteDict[@"text"];
+            quoteItem.author = author;
+            quoteItem.quote = quote;
+            NSLog(@"%@ -%@", author, quote);
+            
+            [quotesList addObject:quoteItem];
+        }
+        
+        self.quotes = quotesList;
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.tableView reloadData];
+        });
+        
+    }] resume];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
